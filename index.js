0f8afcab3d55bab27b07f23b534ee615ea5c56cb
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-01-24 14:15:30 +0900

    Create sample embeds

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const { Webhook } = require('discord-webhook-node');
 const hook = new Webhook("YOUR WEBHOOK URL");
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
 
-const IMAGE_URL = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
-hook.setUsername('Discord Webhook Node Name');
-hook.setAvatar(IMAGE_URL);
+const embed = new MessageBuilder()
+.setTitle('My title here')
+.setAuthor('Author here', 'https://cdn.discordapp.com/embed/avatars/0.png', 'https://www.google.com')
+.setURL('https://www.google.com')
+.addField('First field', 'this is inline', true)
+.addField('Second field', 'this is not inline')
+.setColor('#00b0f4')
+.setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
+.setDescription('Oh look a description :)')
+.setImage('https://cdn.discordapp.com/embed/avatars/0.png')
+.setFooter('Hey its a footer', 'https://cdn.discordapp.com/embed/avatars/0.png')
+.setTimestamp();
 
-hook.send("Hello there!");
+hook.send(embed);
