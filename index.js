af58ea440352db2c24ab3285f614233e5dae4c91
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-02-02 17:00:31 +0900

    Patch axios

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const hook = new Webhook("YOUR WEBHOOK URL");
-const { Webhook, MessageBuilder } = require('discord-webhook-node');
+const {Webhook, MessageBuilder} = require('discord-webhook-node');
+const hook = new Webhook(
+    "https://discord.com/api/webhooks/800281509335400448/YphrCNb2ocEfbim9Hn1Jl4ei4M" +
+    "T5xGzGoxhiFGE3Lw0YdkB4B9ELo3iVtfNHbdIYWBmG"
+);
 
 const embed = new MessageBuilder()
-.setTitle('My title here')
-.setAuthor('Author here', 'https://cdn.discordapp.com/embed/avatars/0.png', 'https://www.google.com')
-.setURL('https://www.google.com')
-.addField('First field', 'this is inline', true)
-.addField('Second field', 'this is not inline')
-.setColor('#00b0f4')
-.setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
-.setDescription('Oh look a description :)')
-.setImage('https://cdn.discordapp.com/embed/avatars/0.png')
-.setFooter('Hey its a footer', 'https://cdn.discordapp.com/embed/avatars/0.png')
-.setTimestamp();
+    .setTitle('새로운 공모전이 올라오다!')
+    .setAuthor('알림봇', 'https://www.campuspick.com/favicon.ico')
+    .setURL('https://www.campuspick.com/contest?category=108')
+    .setColor('#00b0f4')
+    .setFooter('올라온 시간', 'https://www.campuspick.com/favicon.ico')
+    .setTimestamp();
 
 hook.send(embed);
