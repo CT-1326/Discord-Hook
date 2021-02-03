d02242ccd3da3c1155e593beae8e515c5a3b6a87
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-02-03 16:00:40 +0900

    Delete nightmare & Patch cheerio

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const {Webhook, MessageBuilder} = require('discord-webhook-node');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const hook = new Webhook(
     "https://discord.com/api/webhooks/800281509335400448/YphrCNb2ocEfbim9Hn1Jl4ei4M" +
     "T5xGzGoxhiFGE3Lw0YdkB4B9ELo3iVtfNHbdIYWBmG"
 );
 
-const embed = new MessageBuilder()
-    .setTitle('새로운 공모전이 올라오다!')
-    .setAuthor('알림봇', 'https://www.campuspick.com/favicon.ico')
-    .setURL('https://www.campuspick.com/contest?category=108')
-    .setColor('#00b0f4')
-    .setFooter('올라온 시간', 'https://www.campuspick.com/favicon.ico')
-    .setTimestamp();
+axios.get('https://www.campuspick.com/contest?category=108').then(function (params) {
+    // console.log(params.data);
+    const $ = cheerio.load(params.data);
+    const title = $('head > title').text();
+    console.log(title);
+})
 
-hook.send(embed);
+// const embed = new MessageBuilder()
+//     .setTitle('새로운 공모전이 올라오다!')
+//     .setAuthor('알림봇', 'https://www.campuspick.com/favicon.ico')
+//     .setURL('https://www.campuspick.com/contest?category=108')
+//     .setColor('#00b0f4')
+//     .setFooter('올라온 시간', 'https://www.campuspick.com/favicon.ico')
+//     .setTimestamp();
+
+// hook.send(embed);
