2cb3b8c0c00bf7131431e0f7d40d8cf2e6b4e89b
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-02-03 15:26:45 +0900

    Patch nightmare

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log('WWWWWWWWWWWWWWWWWelcome!');
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({show: true})
+
+
+nightmare
+    .goto("https://www.campuspick.com/contest?category=108")
+    .evaluate(
+        () => document.querySelector('#container > div.list').outerText
+    )
+    .end()
+    .then(res => {
+        console.log(res);
+    })
+    .catch(e => {
+        console.error(e);
+    });
