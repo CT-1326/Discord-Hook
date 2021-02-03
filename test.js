e02642a1b44d8f7bba23c66578c9a321650ba5b1
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-02-03 15:44:30 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({})
+// const Nightmare = require('nightmare')
+// const nightmare = Nightmare({})
 
 
-nightmare
-    .goto("https://www.campuspick.com/contest?category=108")
-    .evaluate(
-        () => document.querySelector('#container > div.list').outerText
-    )
-    .end()
-    .then(res => {
-        console.log(res);
-    })
-    .catch(e => {
-        console.error(e);
-    });
+// nightmare
+//     .goto("https://www.campuspick.com/contest?category=108")
+//     .evaluate(
+//         () => document.querySelector('#container > div.list').outerText
+//     )
+//     .end()
+//     .then(res => {
+//         console.log(res);
+//     })
+//     .catch(e => {
+//         console.error(e);
+//     });
+
+const axios = require('axios');
+
+axios.get('https://www.campuspick.com/contest?category=108').then(function (result){
+    console.log(result.headers);
+})
