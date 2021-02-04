ff99700312673514d1a84b5faa010a6114ebeed6
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-02-04 18:56:52 +0900

    Build alert webhook
    alpha version
    
    + patch date-utils

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 const {Webhook, MessageBuilder} = require('discord-webhook-node');
 const axios = require('axios');
-const cheerio = require('cheerio');
+require('date-utils');
 const hook = new Webhook(
     "https://discord.com/api/webhooks/800281509335400448/YphrCNb2ocEfbim9Hn1Jl4ei4M" +
     "T5xGzGoxhiFGE3Lw0YdkB4B9ELo3iVtfNHbdIYWBmG"
 );
 
-axios.get('https://www.campuspick.com/contest?category=108').then(function (params) {
-    // console.log(params.data);
-    const $ = cheerio.load(params.data);
-    const title = $('head > title').text();
-    console.log(title);
-    
-    const embed = new MessageBuilder()
-    .setTitle('새로운 공모전이 올라오다!')
-    .setAuthor(title, 'https://www.campuspick.com/favicon.ico')
-    .setURL('https://www.campuspick.com/contest?category=108')
-    .setColor('#00b0f4')
-    .setFooter('올라온 시간', 'https://www.campuspick.com/favicon.ico')
-    .setTimestamp();
+let config = {
+    method: 'post',
+    url: 'https://api.campuspick.com/find/activity/list?target=1&limit=10&offset=0&categ' +
+            'oryId=108',
+    headers: {}
+};
 
-    hook.send(embed);
-})
+axios(config)
+    .then(function (response) {
+        // console.log(JSON.stringify(response.data)); console.log(getData);
+        let getData = response.data.result.activities;
+        for (let index = 0; index < getData.length; index++) {
+            // console.log(getData[index].image);
+            axios
+                .get(getData[index].image)
+                .then(img => {
+                    const imgDate = img.headers['last-modified'];
+                    // console.log(imgDate);
+                    const today = new Date().toFormat('DDD, DD MMM YYYY');
+                    // console.log(today);
+                    if (imgDate.indexOf(today) != -1) {
+                        const embed = new MessageBuilder()
+                            .setTitle('새로운 공모전이 올라오다!')
+                            .setAuthor("알림봇", 'https://www.campuspick.com/favicon.ico')
+                            .setURL('https://www.campuspick.com/contest?category=108')
+                            .setColor('#00b0f4')
+                            .setFooter('올라온 시간', 'https://www.campuspick.com/favicon.ico')
+                            .setTimestamp();
+
+                        hook.send(embed);
+                    } else {
+                        console.log('Not today!');
+                    }
+                });
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
