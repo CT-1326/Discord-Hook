2a095006e0561a624835c9279c2ad243765780cc
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-02-05 01:08:40 +0900

    fuck

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const {Webhook, MessageBuilder} = require('discord-webhook-node');
 const axios = require('axios');
 require('date-utils');
 const hook = new Webhook(
-    "https://discord.com/api/webhooks/800281509335400448/YphrCNb2ocEfbim9Hn1Jl4ei4M" +
-    "T5xGzGoxhiFGE3Lw0YdkB4B9ELo3iVtfNHbdIYWBmG"
+    "https://discord.com/api/webhooks/806918060837044254/srAZN-mCQwqARND699YXADypbCg7Y_lONUB2VpE7aaiFvQ9iTHclieLBt6jbppXKsXsI"
 );
 
 let config = {
