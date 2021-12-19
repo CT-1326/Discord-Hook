b91f7c59fc986e38fc03ca2cb92de45d6babfb60
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-12-19 23:55:53 +0900

    주석 처리

--- scripts/hotDeal.js
+++ scripts/hotDeal.js
@@ -1,69 +1,69 @@
-const {Webhook, MessageBuilder} = require('discord-webhook-node');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const redis = require('redis');
-const client = redis.createClient(process.env.REDIS_URL);
-require('dotenv').config();
-const hook = new Webhook(process.env.hotDeal_URL);
+const {Webhook, MessageBuilder} = require('discord-webhook-node'); // discord-webhook-node 라이브러리 연동
+const axios = require('axios'); // axios 라이브러리 연동
+const cheerio = require('cheerio'); // cheerio 라이브러리 연동
+const redis = require('redis'); // redis 라이브러리 연동
+const client = redis.createClient(process.env.REDIS_URL); // redis의 연결 클라이언트 변수 생성
+require('dotenv').config(); // 환경변수 라이브러리 연동
+const hook = new Webhook(process.env.hotDeal_URL); // discord-webhook-node 라이브러리의 webhook 변수 생성
 
 axios
-    .get('https://bbs.ruliweb.com/market/board/1020', {timeout: 3000})
+    .get('https://bbs.ruliweb.com/market/board/1020', {timeout: 3000}) // 해당 페이지 크롤링 시도
     .then(function (html) {
         // console.log(html.data);
-        const $ = cheerio.load(html.data);
-        const arr = [];
-        let check = false;
-        client.get('lastID', (err, reply) => {
+        const $ = cheerio.load(html.data); // 크롤링 결과 값을 하나의 변수로 처리 및 활용
+        const arr = []; // 크롤링으로 추출된 게시물 id 값 저장 배열 선언
+        let check = false; // 새로운 게시물이 포함되었는지를 구분하는 Flag 값 선언
+        client.get('lastID', (err, reply) => { // redis 호출 및 시작
             console.log('Redis lastID value:', reply);
             const tableLength = $(
                 '#board_list > div > div.board_main.theme_default > table > tbody > tr'
-            ).length;
-            for (let index = 1; index <= tableLength; index++) {
+            ).length; // 크롤링된 게시판의 게시물 개수를 저장
+            for (let index = 1; index <= tableLength; index++) { // 개수 만큼 반복문 수행
                 let tableName = $(
                     '#board_list > div > div.board_main.theme_default > table > tbody > tr:nth-chil' +
                     'd(' + index + ')'
                 )
                     .attr()
-                    .class;
-                if (tableName == 'table_body') {
+                    .class; // 게시물의 class 명칭을 저장
+                if (tableName == 'table_body') { // 해당 명칭이 업로드 순으로 배치되는 게시물의 class 명칭일 경우
                     let checkID = $(
                         '#board_list > div > div.board_main.theme_default > table > tbody > tr:nth-chil' +
                         'd(' + index + ') > td.id'
                     )
                         .text()
-                        .replace(/\s/g, '');
+                        .replace(/\s/g, ''); // 게시물의 id 값을 추출
                     console.log('Hotdeal ID value:', checkID);
-                    arr.push(checkID);
-                    if (reply.indexOf(checkID) == -1) {
-                        check = true;
+                    arr.push(checkID); // 배열에 저장
+                    if (reply.indexOf(checkID) == -1) { // 해당 id 값이 redis에 저장되지 않은 값일 경우
+                        check = true; // 새로운 게시물이 있음으로 식별
                         const title = $(
                             '#board_list > div > div.board_main.theme_default > table > tbody > tr:nth-chil' +
                             'd(' + index + ') > td.subject > div > a.deco'
-                        ).text();
+                        ).text(); // 게시물의 타이틀 내용을 추출
                         // console.log(title);
                         const embed = new MessageBuilder()
-                            .setTitle(title)
+                            .setTitle(title) // 메시지 타이틀 작성
                             .setAuthor(
                                 "알림봇",
                                 'https://img.ruliweb.com/img/2016/icon/ruliweb_icon_144_144.png'
-                            )
-                            .setURL('https://bbs.ruliweb.com/market/board/1020/read/' + checkID)
-                            .setColor('#181696')
+                            ) // 메시지 아이콘 설정
+                            .setURL('https://bbs.ruliweb.com/market/board/1020/read/' + checkID) // 해당 게시물 주소로 연결
+                            .setColor('#181696') // 메시지 색상 설정
                             .setFooter(
                                 '올라온 시간',
                                 'https://img.ruliweb.com/img/2016/icon/ruliweb_icon_144_144.png'
-                            )
-                            .setTimestamp();
-                        hook.send(embed);
+                            ) // 메시지 footer 작성
+                            .setTimestamp(); // 현재 메시지 작성 시간 저장
+                        hook.send(embed); // 새로운 게시물 알림 메시지 전송
                     }
                 }
             }
             // console.log(arr);
-            if (check) {
+            if (check) { // 새로운 게시물이 있었음을 식별하였다면
                 const redisValue = JSON.stringify(arr);
-                client.set('lastID', redisValue);
+                client.set('lastID', redisValue); // redis의 현재 DB 상태를 저장한 게시물 id 값 배열 내용으로 갱신
             }
-            client.quit();
+            client.quit(); // redis 종료
         });
     })
     .catch(function (e) {
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-const {Webhook, MessageBuilder} = require('discord-webhook-node');
-const axios = require('axios');
-const redis = require('redis');
-const client = redis.createClient(process.env.REDIS_URL);
-require('dotenv').config();
-const hook = new Webhook(process.env.index_URL);
+const {Webhook, MessageBuilder} = require('discord-webhook-node'); // discord-webhook-node 라이브러리 연동
+const axios = require('axios'); // axios 라이브러리 연동
+const redis = require('redis'); // redis 라이브러리 연동
+const client = redis.createClient(process.env.REDIS_URL); // redis의 연결 클라이언트 변수 생성
+require('dotenv').config(); // 환경변수 라이브러리 연동
+const hook = new Webhook(process.env.index_URL); // discord-webhook-node 라이브러리의 webhook 변수 생성
 
 let config = {
     method: 'post',
@@ -11,40 +11,40 @@ let config = {
             'oryId=108',
     headers: {},
     timeout: 1000
-};
+}; // 크롤링 하고자 하는 사이트의 GET 메소드 HTTP요청 환경설정
 
 axios(config)
     .then(function (response) {
         // console.log(JSON.stringify(response.data));
-        let getData = response.data.result.activities;
+        let getData = response.data.result.activities; // 크롤링 결과 값을 변수 처리
         console.log(getData);
-        let check = false;
-        let arr = [];
-        client.get('idKey', (err, params) => {
+        let check = false; // 새로운 게시물이 포함되었는지를 구분하는 Flag 값 선언
+        let arr = []; // 크롤링으로 추출된 게시물 id 값 저장 배열 선언
+        client.get('idKey', (err, params) => { // redis 호출 및 시작
             console.log('Redis idKey value:', params);
-            for (let index = 0; index < getData.length; index++) {
+            for (let index = 0; index < getData.length; index++) { // 크롤링으로 추출된 게시물 개수 만큼 반복문 수행
                 console.log('gongmo ID value:', getData[index].id);
-                arr.push(getData[index].id);
-                if (params.indexOf(getData[index].id) == -1) {
-                    check = true;
+                arr.push(getData[index].id); // 크롤링 게시물 id 값을 배열에 저장
+                if (params.indexOf(getData[index].id) == -1) { // 현재의 게시물 id 값이 redis에 저장되지 않은 값일 경우
+                    check = true; // 새로운 게시물이 있음으로 식별
                     const embed = new MessageBuilder()
-                        .setTitle('새로운 공모전이 올라오다!')
-                        .setAuthor("알림봇", 'https://www.campuspick.com/favicon.ico')
-                        .setURL('https://www.campuspick.com/contest/view?id=' + getData[index].id)
-                        .setColor('#00b0f4')
-                        .setFooter('올라온 시간', 'https://www.campuspick.com/favicon.ico')
-                        .setTimestamp();
-                    hook.send(embed);
+                        .setTitle('새로운 공모전이 올라오다!') // 메시지 타이틀 작성
+                        .setAuthor("알림봇", 'https://www.campuspick.com/favicon.ico') // 메시지 아이콘 설정
+                        .setURL('https://www.campuspick.com/contest/view?id=' + getData[index].id) // 해당 게시물 주소로 연결
+                        .setColor('#00b0f4') // 메시지 색상 설정
+                        .setFooter('올라온 시간', 'https://www.campuspick.com/favicon.ico') // 메시지 footer 작성
+                        .setTimestamp(); // 현재 메시지 작성 시간 저장
+                    hook.send(embed); // 새로운 게시물 알림 메시지 전송
                 } else {
                     console.log('Not today!');
                 }
             }
             // console.log(check);
-            if (check) {
+            if (check) { // 새로운 게시물이 있었음을 식별하였다면
                 const redisValue = JSON.stringify(arr);
-                client.set('idKey', redisValue);
+                client.set('idKey', redisValue); // redis의 현재 DB 상태를 저장한 게시물 id 값 배열 내용으로 갱신
             }
-            client.quit();
+            client.quit(); // redis 종료
         });
     })
     .catch(function (error) {
