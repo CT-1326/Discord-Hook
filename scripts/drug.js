c1726f62da03bbdde828f8e128dbbbf8c770a0c8
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-12-13 13:31:13 +0900

    #18 - drug alert 변경

--- scripts/drug.js
+++
@@ -1,9 +0,0 @@
-const {Webhook, MessageBuilder} = require('discord-webhook-node');
-const hook = new Webhook(process.env.drug_URL); //  discord-webhook-node 라이브러리의 webhook 변수 생성
-
-hook.setUsername('Drug BOT');
-const embed = new MessageBuilder()
-    .setTitle('약 먹자')
-    .setColor('#FF0000')
-    .setTimestamp(); // 지정된 시간에 메시지가 도착하였는지를 확인하기 위한 시간대 출력
-hook.send(embed);
