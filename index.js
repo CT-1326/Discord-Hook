65eadce916c0b75eecda3538c3a1157d2779da08
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2024-07-01 18:11:20 +0900

    #24 1,2번 진행완료

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const schedule = require('node-schedule');
+require('dotenv').config();
 const drug = require('./scripts/drug');
-const gongmo = require('./scripts/gongmo');
-const hotDeal = require('./scripts/hotDeal');
 
-app.get([
-    "/", "/:name"
-], (req, res) => {
-    greeting = "<h1>Hello From Node on Fly!</h1>";
-    name = req.params["name"];
-    if (name) {
-        res.send(greeting + "</br>and hello to " + name);
-    } else {
-        res.send(greeting);
-    }
+schedule.scheduleJob('0 22 * * *', function () {
+    drug();
+});
+schedule.scheduleJob('0 11 * * *', function () {
+    drug();
 });
 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 app.listen(port, () => {
-    /* 리스너에 시간대별 스케줄링 작성 */
-    schedule.scheduleJob('0 22 * * *', function () {
-        drug();
-    });
-    schedule.scheduleJob('0 11 * * *', function () {
-        drug();
-    });
-
-    schedule.scheduleJob('*/10 * * * *', function () {
-        hotDeal();
-    });
-
-    // schedule.scheduleJob('0 * * * *', function () {
-    //     gongmo();
-    // });
+  console.log(`Example app listening on port ${port}`);
 });
--- scripts/drug.js
+++ scripts/drug.js
@@ -1,11 +1,12 @@
 const {Webhook, MessageBuilder} = require('discord-webhook-node');
-const hook = new Webhook(process.env.DRUG_URL); // 약 채널 webhook 경로 등록
+const hook = new Webhook(process.env.DRUG_URL); // webhook 경로 등록
 
 module.exports = function () {
-    hook.setUsername('Drug BOT');
+    hook.setUsername('Drug BOT'); // BOT 이름 작성
+    /* 메시지 내용 작성 */
     const embed = new MessageBuilder()
         .setTitle('약 먹자')
         .setColor('#FF0000')
         .setTimestamp(); // 지정된 시간에 메시지가 도착하였는지를 확인하기 위한 시간대 출력
-    hook.send(embed);
+    hook.send(embed); // 메시지 전송
 }
