bcfc4dc59a87faba1ce5e9013309b7f0467e7e1a
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2024-07-02 15:51:41 +0900

    #24 3번 진행완료 및 배포 설정 재조정
    앱 자동중단 방지

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const port = 3000;
 const schedule = require('node-schedule');
 require('dotenv').config();
 const drug = require('./scripts/drug');
+const hotDeal = require('./scripts/hotDeal');
 
 schedule.scheduleJob('0 22 * * *', function () {
-    drug();
+    drug()
 });
 schedule.scheduleJob('0 11 * * *', function () {
-    drug();
+    drug()
 });
-
+// schedule.scheduleJob('*/10 * * * *', function () {
+//     hotDeal()
+// });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  hotDeal();
+    res.send('Hello World!');
 });
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
--- scripts/drug.js
+++ scripts/drug.js
@@ -2,7 +2,7 @@ const {Webhook, MessageBuilder} = require('discord-webhook-node');
 const hook = new Webhook(process.env.DRUG_URL); // webhook 경로 등록
 
 module.exports = function () {
-    hook.setUsername('Drug BOT'); // BOT 이름 작성
+    hook.setUsername('Drug Alert'); // BOT 이름 작성
     /* 메시지 내용 작성 */
     const embed = new MessageBuilder()
         .setTitle('약 먹자')
