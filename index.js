b723e9da5bbae5d1e7bb66ff17e373e6722d86dc
Park sejun <smw7567@gmail.com>
2025-02-14 22:52:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ schedule.scheduleJob('*/10 * * * *', function () {
     hotDeal();
 });
 
-schedule.scheduleJob('0 24 * * *', function () {
+schedule.scheduleJob('0 0 * * *', function () {
     jpJob();
 });
 
